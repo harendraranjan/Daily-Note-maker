5eda550f1be0df974a03d67df173610eec4b0fd4
harendraranjan <ranjanharendr@gmail.com>
2024-07-31 23:20:50 +0530

    Change some file

--- src/main/java/com/Enote/config/MyConfiguration.java
+++ src/main/java/com/Enote/config/MyConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -37,7 +36,8 @@ public class MyConfiguration {
 		http.csrf((csrf) -> csrf.disable())
 				.authorizeHttpRequests()
 					.requestMatchers("/user/**").hasRole("USER")
-					.requestMatchers("/**").permitAll().and()
+					.requestMatchers("/**").permitAll()
+					.and()
 					.formLogin().permitAll();
 					
 	return	http.build();
